be18f61b038c7902a171194e55267303e31eb91f
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-04-28 17:15:42 -0500

    Implement Sayo::SetLight

---
+++ src/HL/commands.cpp
@@ -0,0 +1,21 @@
+#include "../../naraapi.h"
+
+using namespace Nara;
+
+
+void Sayo::SetLight(int key, int fn, Color color)
+{
+	// Initialize package
+	struct API_CMD_0X11 package = { 0 };
+	
+	// Read the current values
+	LL::light_config_read(device, key, &package);
+
+	// Modify the package
+	package.led_fn[fn].r = color.r;
+	package.led_fn[fn].g = color.g;
+	package.led_fn[fn].b = color.b;
+
+	// Write new values
+	LL::light_config_write(device, key, &package);
+}
