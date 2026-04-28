e176601619f569ed0dd1045dbb433c9f86910777
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-04-28 17:29:32 -0500

    Implement light mode functions

--- src/HL/commands.cpp
+++ src/HL/commands.cpp
@@ -35,3 +35,31 @@ Color Sayo::ReadLight(int key, int fn)
 		package.led_fn[fn].b
 	};
 }
+
+
+void Sayo::SetLightMode(int key, int fn, int mode)
+{
+	// Initialize package
+	struct API_CMD_0X11 package = { 0 };
+	
+	// Read the current values
+	LL::light_config_read(device, key, &package);
+
+	// Modify the package
+	package.led_fn[fn].led_mode = mode;
+
+	// Write new values
+	LL::light_config_write(device, key, &package);
+}
+
+int Sayo::ReadLightMode(int key, int fn)
+{
+	// Initialize package
+	struct API_CMD_0X11 package = { 0 };
+	
+	// Read the current values
+	LL::light_config_read(device, key, &package);
+	
+	// Return values
+	return package.led_fn[fn].led_mode;
+}
