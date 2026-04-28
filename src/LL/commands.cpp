d0b2b7b5a3754efe87abab9187f531c6c81f8cc5
AppleJuiceNerd <81881221+AppleJuiceNerd@users.noreply.github.com>
2026-04-28 16:46:28 -0500

    Change set_key_lights to "return" a package

--- src/LL/commands.cpp
+++ src/LL/commands.cpp
@@ -16,29 +16,37 @@ uint16_t Nara::LL::checksum(uint8_t *data, int length)
 }
 
 
-void Nara::LL::set_key_lights(hid_device *sayo, uint8_t key, struct API_CMD_0X11 req_data, uint8_t *result)
+void Nara::LL::set_key_lights(hid_device *sayo, uint8_t key, struct API_CMD_0X11 *pkg)
 {
+	// Set up headers
 	struct PKT_HEADER pkt = { 0x22, NARA_ECHO_CODE, 0 };
 	struct CMD_HEADER cmd = { 60, 0x11, key };
+
+	// Set up byte array
 	uint8_t data[1024] = { 0 };
 
-	memcpy(&data, &pkt, sizeof(pkt));
+	// Copy packet header, command header, and package to byte array
+	memcpy(data, &pkt, sizeof(pkt));
 	memcpy(&data[4], &cmd, sizeof(cmd));
-	memcpy(&data[8], &req_data, sizeof(req_data));
+	memcpy(&data[8], pkg, sizeof(*pkg));
 
-	pkt.checksum = checksum(data, 1000);
+	// Calculate checksum
+	pkt.checksum = checksum(data, 1024);
 	
+	// Copy checksum to byre array
 	memcpy(&data, &pkt, sizeof(pkt));
 
+	// Send packet to Sayodevice
 	hid_write(sayo, data, 1024);
 	
-
+	// Wait for a response
 	// NOTE: May be prone to infinite loops
 	do {
 		hid_read(sayo, data, 1024);
 	} while (data[1] != NARA_ECHO_CODE);
 
-	memcpy(&data, &result, 1024);
+	// Copy package data to package
+	memcpy(pkg, &data[8], sizeof(*pkg));
 }
 
 void Nara::LL::read_key_lights(hid_device *sayo, uint8_t key, uint8_t *result)
